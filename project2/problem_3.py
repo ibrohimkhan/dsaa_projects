9b290f07a61814a8546c9d53ed85d605e65d8d56
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2020-11-05 01:41:24 +0500

    completed implementation of problem_3

---
+++ project2/problem_3.py
@@ -0,0 +1,136 @@
+import heapq
+import sys
+
+
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        if other is None:
+            return False
+
+        if not isinstance(other, Node):
+            return False
+
+        return self.freq < other.freq
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+
+        if not isinstance(other, Node):
+            return False
+
+        return self.char == other.char
+
+    def __repr__(self):
+        return f"{self.char}: {self.freq}"
+
+
+def get_frequency(text):
+    frequency = {}
+    for char in text:
+        if not char in frequency:
+            frequency[char] = 0
+        frequency[char] += 1
+
+    return frequency
+
+
+def create_heap(frequency):
+    heap = []
+    for key in frequency:
+        node = Node(key, frequency[key])
+        heapq.heappush(heap, node)
+
+    return heap
+
+
+def merge_nodes(heap):
+    while len(heap) > 1:
+        first_node = heapq.heappop(heap)
+        second_node = heapq.heappop(heap)
+
+        freq_sum = first_node.freq + second_node.freq
+        merged_node = Node(None, freq_sum)
+        merged_node.left = first_node
+        merged_node.right = second_node
+
+        heapq.heappush(heap, merged_node)
+
+
+def binaryze(heap):
+    root = heapq.heappop(heap)
+    codes = {}
+    byte_codes = ''
+
+    def add_binary_code(root, byte_codes):
+        if root is None:
+            return
+
+        if root.char is not None:
+            codes[root.char] = byte_codes
+            return
+
+        add_binary_code(root.left, byte_codes + '0')
+        add_binary_code(root.right, byte_codes + '1')
+
+    add_binary_code(root, byte_codes)
+    return codes
+
+
+def reverse(codes):
+    reversed_codes = {}
+    for key in codes:
+        reversed_codes[codes[key]] = key
+
+    return reversed_codes
+
+
+def huffman_encoding(data):
+    freq = get_frequency(data)
+    heap = create_heap(freq)
+    merge_nodes(heap)
+
+    codes = binaryze(heap)
+    encoded_text = ''
+    for char in data:
+        encoded_text += codes[char]
+
+    return encoded_text, codes
+
+
+def huffman_decoding(data, tree):
+    byte_codes = ''
+    decoded_text = ''
+
+    reverse_codes = reverse(tree)
+    for bit in data:
+        byte_codes += bit
+        if byte_codes in reverse_codes:
+            char = reverse_codes[byte_codes]
+            decoded_text += char
+            byte_codes = ''
+
+    return decoded_text
+
+
+if __name__ == '__main__':
+    a_great_sentence = "The bird is the word"
+
+    print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print("The content of the encoded data is: {}\n".format(decoded_data))
